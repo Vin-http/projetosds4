a76a6f04a229f2fe37e5e0f66b64cd5fc2f56587
Vinicius Alves <viniciusasantos01@gmail.com>
2021-09-08 02:38:32 -0300

    Projetos criados

---
+++ backend/src/test/java/com/devsuoerior/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuoerior.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
