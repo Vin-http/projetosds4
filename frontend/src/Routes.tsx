edd65d1d67b6c41e364550b9f4010ca75c1e5724
Vinicius Alves <viniciusasantos01@gmail.com>
2021-09-12 15:35:44 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,22 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+       <div>
+           <BrowserRouter>
+           <switch>
+               <Route path="/" exact>
+                   <Home />
+               </Route>
+               <Route path="/dashboard">
+                   <Dashboard />
+               </Route>
+           </switch>
+           </BrowserRouter>
+       </div>
+    );
+}
+
+export default Routes;
